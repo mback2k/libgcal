faae34a6b0750fb273443a6d9d5cea755f7625cd
Adenilson Cavalcanti <adenilson.silva@indt.org.br>
2008-05-08 15:01:05 -0400

    Adding a stress test: it will add near 400 events along a month and delete then.
    Good: add worked fine.
    
    Bad: delete fails (actually parsing all events seems to fail in function
    'extract_all_entries').
    
    Ugly: it took more than 20 minutes to run the test (fscking slow internet
    connection of Manaus - AM - Brazil... welcome to the jungle baby!).

--- utests/utest_edit.c
+++ utests/utest_edit.c
@@ -11,6 +11,7 @@
 #include "xml_aux.h"
 #include "gcal_parser.h"
 #include <string.h>
+#include <unistd.h>
 
 static struct gcal_resource *ptr_gcal = NULL;
 
@@ -139,10 +140,78 @@ START_TEST (test_edit_delete)
 END_TEST
 
 
+
+START_TEST (test_edit_stress)
+{
+	int result, i, j, total;
+	struct gcal_entries event;
+	char *format_start, *format_end;
+	char start_buffer[256], end_buffer[256];
+	char number_buffer1[4], number_buffer2[4];
+	struct gcal_entries *entries;
+
+	event.title = "A new event: stress test";
+	event.content = "Here goes the description of my new event";
+	format_start = "2008-05-%sT%s:00:00.000Z";
+	format_end = "2008-05-%sT%s:00:00.000Z";
+	event.where = "someplace";
+	/* TODO: think in a better way to describe the status, maybe use
+	 * a set of strings.
+	 */
+	event.status = "confirmed";
+
+	result = gcal_get_authentication("gcalntester", "77libgcal", ptr_gcal);
+	fail_if(result == -1, "Authentication should work.");
+
+
+	/* Whole month loop */
+	for (i = 1; i < 31; ++i) {
+		/* whole day loop */
+		for (j = 4; j < 23; ++j) {
+			snprintf(number_buffer1, sizeof(number_buffer1) - 1,
+				 "%02d", i);
+			snprintf(number_buffer2, sizeof(number_buffer2) - 1,
+				 "%02d", j);
+			snprintf(start_buffer, sizeof(start_buffer) - 1,
+				 format_start, number_buffer1, number_buffer2);
+
+			snprintf(number_buffer2, sizeof(number_buffer2) - 1,
+				 "%02d", j + 1);
+			snprintf(end_buffer, sizeof(end_buffer) - 1,
+				 format_end, number_buffer1, number_buffer2);
+
+			event.dt_start = start_buffer;
+			event.dt_end = end_buffer;
+			result = gcal_create_event(&event, ptr_gcal);
+			fail_if(result == -1, "Failed creating a new event!"
+				" Loop is i = %d\tj = %d", i, j);
+		}
+	}
+
+	/* Give me some time to check if everything was added */
+	sleep(40);
+
+	/* Delete all test events */
+	result = gcal_dump(ptr_gcal);
+	fail_if(result == -1, "Failed dumping events");
+
+	entries = gcal_get_entries(ptr_gcal, &total);
+	fail_if(entries == NULL, "Failed extracting entries");
+	for (i = 0; i < total; ++i)
+		if (!strcmp(entries[i].title, event.title)) {
+			result = gcal_delete_event(&entries[i], ptr_gcal);
+			fail_if(result == -1, "Failed deleting event!");
+		}
+
+
+}
+END_TEST
+
+
 TCase *edit_tcase_create(void)
 {
 	TCase *tc = NULL;
-	int timeout_seconds = 90;
+	int timeout_seconds = 1200;
 	tc = tcase_create("edit");
 	tcase_add_checked_fixture(tc, setup, teardown);
 	tcase_set_timeout (tc, timeout_seconds);
@@ -150,6 +219,7 @@ TCase *edit_tcase_create(void)
 	tcase_add_test(tc, test_edit_xmlres);
 	tcase_add_test(tc, test_edit_xml);
 	tcase_add_test(tc, test_edit_delete);
+	tcase_add_test(tc, test_edit_stress);
 	return tc;
 }
 
