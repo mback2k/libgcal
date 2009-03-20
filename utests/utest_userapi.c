c54af3ceb286dbdfb13d2bf80b16e53d383308ff
Adenilson Cavalcanti <adenilson@macblack.(none)>
2009-03-20 19:26:43 -0400

    Cleaning up compiler warning (unused variable).

--- utests/utest_userapi.c
+++ utests/utest_userapi.c
@@ -427,7 +427,7 @@ START_TEST (test_contact_photo)
 	gcal_contact_t contact, tmp;
 	char *photo_data;
 	struct gcal_contact_array contact_array;
-	int result, i;
+	int result;
 
 	if (find_load_photo("/utests/images/gromit.jpg",  &photo_data, &result))
 		fail_if(1, "Cannot load photo!");
